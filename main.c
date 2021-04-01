84bafa3363c5ffd55a93e0d5fbaeb26546c0f4e0
Phoenix <PhoenixWT@protonmail.com>
2021-03-31 23:57:36 -0400

    use getline() and format code

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 
 void print_prompt1(void)
 {
     char hostname[100];
-    gethostname(hostname, sizeof(hostname)); 
-    char name[300]; 
-    getlogin_r(name, 300); 
-    printf("%s@%s%c ", name, hostname, '$'); 
+    gethostname(hostname, sizeof(hostname));
+    char name[300];
+    getlogin_r(name, 300);
+    printf("%s@%s%c ", name, hostname, '$');
 }
 
 void print_prompt2(void)
@@ -18,45 +18,22 @@ void print_prompt2(void)
     fprintf(stderr, "> ");
 }
 
-
-char *read_cmd() 
+char *read_cmd()
 {
-    char *str;
-    char ch = 'p';
-    char temp[1024]; 
-    int length = 0;  
-    while (1)
-    {
-        ch = getchar();
-        temp[length] = ch;
-        length++;
+    char *line = NULL;
+    size_t cap = 0;
+    ssize_t len;
 
-        if (ch == '\n' )
-        {
-            if (temp[length - 2] == '\\') 
-            {
-                print_prompt2();
-                continue;
-            } else 
-            {
-                break;
-            }
-        }
-
-    }
-    temp[length] = '\0';
-    str = malloc(sizeof(char) * length);
-    int i = 0; 
-    while(temp[i] != '\0') 
+    if ((len = getline(&line, &cap, stdin)) < 0)
     {
-        str[i] = temp[i];
-        i++;         
+        // exit on EOF (^D/Ctrl+D)
+        if (feof(stdin))
+            exit(0);
+        exit(1);
     }
-    str[i] = '\0';
-    return str; 
+
+    return line;
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -69,26 +46,26 @@ int main(int argc, char **argv)
 
         cmd = read_cmd();
 
-        if(!cmd)
+        if (!cmd)
         {
             exit(EXIT_SUCCESS);
         }
 
-        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        if (cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
         {
             free(cmd);
             continue;
         }
 
-        if(strcmp(cmd, "exit\n") == 0)
+        if (strcmp(cmd, "exit\n") == 0)
         {
             free(cmd);
             break;
         }
         system(cmd);
-        
+
         free(cmd);
-        } while(1);
+    } while (1);
 
     return 0;
 }
